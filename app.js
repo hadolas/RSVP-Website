4b7fd2260bd7ff0991b2f57dc0dc68c9a9c9ca0c
Hadolas <hadiyahmughal@gmail.com>
2019-10-20 23:46:35 +0100

    Move content from wedding_rsvp.html to views/index.ejs

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var express = require("express");
 var app = express();
 
 app.get("/", function(req, res) {
-	res.send("This is the homepage");
+	// res.send("This is the homepage");
+	res.render("index.ejs");
 });
 
 app.listen(3000, function() {
